29311fee7f8df3377619a4131905c207793bbb48
peet <petrounknown@gmail.com>
2025-01-29 11:59:23 +0300

    more AirSpace

--- python.py
+++ python.py
@@ -10,7 +10,7 @@ class App(ctk.CTk):
         self.title("Space Blaster")
         self.geometry("-5-0")
         self.resizable(0, True)
-        self.canvas_size = 400
+        self.canvas_size = 800
         # self.iconpath = ImageTk.PhotoImage(file=os.path.join("/assets/logo.png"))
         # self.root.wm_iconbitmap()
         # self.root.iconphoto(False, self.iconpath)
@@ -22,11 +22,15 @@ class App(ctk.CTk):
         self.canvasWidget.grid(row=0,column=0)
 
         self.canvasWidget.bind("<Button-1>", self.on_click)
-        self.canvasWidget.bind('<Right>',lambda event: self.moveJet(event,"right"))
-        self.canvasWidget.bind('<Left>',lambda event: self.moveJet(event,"left"))
+        self.canvasWidget.bind('<Right>',self.flyRight)
+        self.canvasWidget.bind('<Left>',self.flyLeft)
+        # self.canvasWidget.bind('<Right>',lambda event: self.moveJet(event,"right"))
+        # self.canvasWidget.bind('<Left>',lambda event: self.moveJet(event,"left"))
+        self.canvasWidget.bind('<Up>',lambda event: self.moveJet(event,"up"))
+        self.canvasWidget.bind('<Down>',lambda event: self.moveJet(event,"down"))
         self.canvasWidget.focus_set()
         # self.canvas.create_arc(0, 0, 300, 300,width=1,start=0, extent=-180)
-        canvas=self.canvas_size
+        canvas=100
         flat_coordinates = [
             0.5*canvas,0.05*canvas,
             0.54*canvas,0.17*canvas,
@@ -81,8 +85,17 @@ class App(ctk.CTk):
             self.canvasWidget.move(self.jet, 30, 0)
         elif direction == "left":
             self.canvasWidget.move(self.jet, -30, 0)
+        elif direction == "up":
+            self.canvasWidget.move(self.jet, 0, -30)
+        elif direction == "down":
+            self.canvasWidget.move(self.jet, 0, 30)
         else:
             print("Wrong Direction")
+
+    def flyRight(self,event):
+        self.canvasWidget.move(self.jet, 30, 0)
+    def flyLeft(self,event):
+        self.canvasWidget.move(self.jet, -30, 0)
         
     
     def on_click(self, event):
